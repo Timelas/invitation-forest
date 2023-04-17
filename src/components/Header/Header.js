98f1b048adc24abc70fca23f758a51c4bdb0174b
Timelas <lom.a.s@icloud.com>
2023-04-17 12:19:44 +0300

    правки

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { useMediaQuery } from 'usehooks-ts';
 import { dayWeek, year, month } from '../../vendor/const';
 import "./Header.css";
 import forestcloud from "../../images/forestcloud.jpg";
-import names from "../../images/names.svg";
+import namesH from "../../images/names.svg";
+import namesV from "../../images/namesV.svg";
 import day from "../../images/day.svg";
 
 function Header() {
+  const resize = useMediaQuery('(min-width: 700px)');
+
   return (
     <section className="header" id="home">
       <div className="header__desctop">
         <img src={forestcloud} alt="облака" className="header__background"></ img>
-        <img src={names} alt="имена" className="header__names"></ img>
+        { 
+        resize ? (
+          <img src={namesH} alt="имена" className="header__names"></ img>
+        ):(
+          <img src={namesV} alt="имена" className="header__names"></ img>
+        ) }
         <div className="header__date">
           <div className="header__date_info_line"><p className="header__text">{dayWeek}</p></div>
           <div className="header__block-line">
